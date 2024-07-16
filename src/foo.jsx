c8544a44617214a18250e81a9d84443c02be76b6
sungpaks <emforhs03150@gmail.com>
2024-07-16 21:36:37 +0900

    [update] prettier, eslint, material ui, typescript 등 의존성 추가, MUI 컴포넌트 예시 작성

---
+++ src/foo.jsx
@@ -0,0 +1,11 @@
+import { Box, Button } from "@mui/material";
+
+export default function Foo() {
+  return (
+    <>
+      <Box>
+        <Button variant="outlined">Button</Button>
+      </Box>
+    </>
+  );
+}
