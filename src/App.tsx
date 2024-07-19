b35a60a54ce7875ac42c8e5078c572e220fda0d1
doyun5565 <kdy__35@naver.com>
2024-07-19 20:34:28 +0900

    feat: 로그인 화면 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
 import * as React from "react";
 import { useState } from "react";
 import "./App.css";
-import { Autocomplete, Button, TextField } from "@mui/material";
-import Foo from "./foo";
+import Login from "./Login";
 
 interface AutocompletionOption {
   label: string;
 }
 
 function App() {
-  const [count, setCount] = useState(0);
-  const options: AutocompletionOption[] = [
-    { label: "어벤져스" },
-    { label: "인사이드아웃" },
-    { label: "데드풀" },
-  ];
-
   return (
     <>
-      <Button variant="contained">HELLO!!</Button>
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={options}
-        sx={{ width: 300 }}
-        renderInput={(params) => (
-          <TextField {...params} label="Movie"></TextField>
-        )}
-      ></Autocomplete>
-      <Foo />
+      <Login />
     </>
   );
 }
