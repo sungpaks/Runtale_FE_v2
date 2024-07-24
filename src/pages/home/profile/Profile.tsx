f4edc52a1f3cce161fd81729d3bb0c88c513d174
sungpaks <emforhs03150@gmail.com>
2024-07-24 19:13:49 +0900

    [style] 프로필 카드 스타일 수정

--- src/pages/home/profile/Profile.tsx
+++ src/pages/home/profile/Profile.tsx
@@ -17,7 +17,17 @@ export default function Profile(
 				height: "300px",
 			}}
 		>
-			<Box sx={{ height: "120px" }}>달팽이 그림..</Box>
+			<Box
+				sx={{
+					height: "120px",
+					border: "1px dashed gray",
+					ml: 2,
+					mr: 2,
+					borderRadius: 3,
+				}}
+			>
+				달팽이 그림.. <br /> 외곽선은 구분용으로 그려놓음
+			</Box>
 			<h3>세종이</h3>
 			<Stack textAlign={"left"} spacing={0}>
 				<ListItem>레벨</ListItem>
