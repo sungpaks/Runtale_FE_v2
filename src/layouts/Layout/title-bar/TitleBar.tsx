d0b9e7b330291972a7aa8962872c437dee6bef2b
sungpaks <emforhs03150@gmail.com>
2024-07-28 14:44:37 +0900

    [feat] 상단 바 뒤로가기 버튼 추가

--- src/layouts/Layout/title-bar/TitleBar.tsx
+++ src/layouts/Layout/title-bar/TitleBar.tsx
@@ -1,19 +1,30 @@
-import { useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate } from "react-router-dom";
 import "./TitleBar.css";
 
-export default function TitleBar({ hasPreviousButton }: { hasPreviousButton: boolean; }) {
-    const navigate = useNavigate();
+export default function TitleBar({
+	hasPreviousButton,
+}: {
+	hasPreviousButton: boolean;
+}) {
+	const navigate = useNavigate();
 
-    const handleTitleClick = () => {
-        navigate('/home');
-    };
+	const handleTitleClick = () => {
+		navigate("/home");
+	};
 
-    return (
-        <div className="title-bar">
-            {hasPreviousButton ? <div>^</div> : undefined}
-            <h2 onClick={handleTitleClick} style={{ cursor: 'pointer' }}>
-                👟RunTale
-            </h2>
-        </div>
-    );
+	return (
+		<div className="title-bar">
+			{hasPreviousButton ? (
+				<div
+					className={"back-button"}
+					onClick={() => {
+						navigate(-1);
+					}}
+				></div>
+			) : undefined}
+			<h2 onClick={handleTitleClick} style={{ cursor: "pointer" }}>
+				👟RunTale
+			</h2>
+		</div>
+	);
 }
