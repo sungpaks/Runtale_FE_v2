0cc86eb8d489f0f9b3820a43984e5d0ad64b8aef
sungpaks <emforhs03150@gmail.com>
2024-07-28 17:43:12 +0900

    [feat] 러닝 트래커 기능 개발 단계 : 실시간 위치 반영, 지나온 경로 다각선 표현, 테스트 모드 등

---
+++ src/pages/running/tracker/Tracker.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef, useState } from "react";
+import { MapMarker, Polyline } from "react-kakao-maps-sdk";
+
+interface PathType {
+	lat: number;
+	lng: number;
+}
+
+export default function Tracker({
+	latitude,
+	longitude,
+}: {
+	latitude: number;
+	longitude: number;
+}) {
+	const [path, setPath] = useState<PathType[]>([]);
+	useEffect(() => {
+		setPath((prev) => [...prev, { lat: latitude, lng: longitude }]);
+	}, [latitude, longitude]);
+	return (
+		<>
+			<MapMarker position={{ lat: latitude, lng: longitude }}></MapMarker>
+			<Polyline
+				path={path}
+				strokeWeight={10} // 선의 두께 입니다
+				strokeColor={"red"} // 선의 색깔입니다
+				strokeOpacity={0.7} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+				strokeStyle={"solid"} // 선의 스타일입니다
+			/>
+			<Polyline
+				path={path}
+				strokeWeight={5} // 선의 두께 입니다
+				strokeColor={"yellow"} // 선의 색깔입니다
+				strokeOpacity={0.7} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+				strokeStyle={"solid"} // 선의 스타일입니다
+			/>
+		</>
+	);
+}
