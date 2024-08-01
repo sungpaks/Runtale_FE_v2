aff71d1f47024bf0838e737b52b58803f7a37e41
sungpaks <emforhs03150@gmail.com>
2024-08-01 16:29:43 +0900

    [update] "이달의 기록" 페이지 데이터 fetch

--- src/pages/statistics/Statistics.tsx
+++ src/pages/statistics/Statistics.tsx
@@ -1,8 +1,35 @@
 import { Box, Grid } from "@mui/material";
 import AnimalCrawls from "../../components/AnimalCrawls";
 import Title from "../../components/Title";
+import { getRunningRecordMonthly } from "../../api/api";
+import { useQuery } from "react-query";
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../../context/AuthContext";
 
 export default function Statistics() {
+	const { userId } = useContext(AuthContext);
+	const { isSuccess, data } = useQuery({
+		queryKey: "monthlyRecords",
+		queryFn: async () => await getRunningRecordMonthly({ userId }),
+	});
+
+	if (!isSuccess) return;
+	const {
+		averagePace,
+		runningList,
+		targetDistanceAchievedCount,
+		targetPaceAchievedCount,
+		totalDistance,
+		totalRunningCount,
+	} = data.data.data;
+	console.log(
+		averagePace,
+		runningList,
+		targetDistanceAchievedCount,
+		targetPaceAchievedCount,
+		totalDistance,
+		totalRunningCount,
+	);
 	return (
 		<Box p={1}>
 			<Title
@@ -24,7 +51,9 @@ export default function Statistics() {
 						borderRadius: 3,
 						backgroundColor: "lightgray",
 					}}
-				></Box>
+				>
+					여기에는 차트를 넣어요
+				</Box>
 				<Grid
 					container
 					spacing={2}
@@ -38,34 +67,42 @@ export default function Statistics() {
 						<div>
 							<h4 style={{ margin: 0 }}>달린 거리</h4>
 							<p style={{ margin: 0, fontSize: "1.5rem" }}>
-								10KM
+								{totalDistance}km
 							</p>
 						</div>
 					</Grid>
 					<Grid xs={4} item>
 						<div>
 							<h4 style={{ margin: 0 }}>러닝 횟수</h4>
-							<p style={{ margin: 0, fontSize: "1.5rem" }}>3</p>
+							<p style={{ margin: 0, fontSize: "1.5rem" }}>
+								{totalRunningCount}
+							</p>
 						</div>
 					</Grid>
 					<Grid xs={4} item>
 						<div>
 							<h4 style={{ margin: 0 }}>평균 페이스</h4>
 							<p style={{ margin: 0, fontSize: "1.5rem" }}>
-								5m 50s
+								{averagePace}
 							</p>
 						</div>
 					</Grid>
 					<Grid xs={4} item>
 						<div>
 							<h4 style={{ margin: 0 }}>시나리오 완수</h4>
-							<p style={{ margin: 0, fontSize: "1.5rem" }}>2</p>
+							<p style={{ margin: 0, fontSize: "1.5rem" }}>
+								{
+									targetDistanceAchievedCount /* 이거 맞나? "시나리오 완수 횟수"는 없긴 함 지금 */
+								}
+							</p>
 						</div>
 					</Grid>
 					<Grid xs={5} item>
 						<div>
 							<h4 style={{ margin: 0 }}>목표 페이스 달성</h4>
-							<p style={{ margin: 0, fontSize: "1.5rem" }}>2</p>
+							<p style={{ margin: 0, fontSize: "1.5rem" }}>
+								{targetPaceAchievedCount}
+							</p>
 						</div>
 					</Grid>
 				</Grid>
