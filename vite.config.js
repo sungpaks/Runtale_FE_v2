484b28abbfb6f0f6bf588e4546ba2350ccd20447
sungpaks <emforhs03150@gmail.com>
2024-08-03 18:11:21 +0900

    [fix] 지도 크기 줄이고 test버튼 숨김

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://43.201.149.65',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''), // '/api'를 제거하여 실제 경로로 만듭니다.
-      },
-    },
-  },
-  plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://43.201.149.65",
+				changeOrigin: true,
+				secure: false,
+				rewrite: (path) => path.replace(/^\/api/, ""), // '/api'를 제거하여 실제 경로로 만듭니다.
+			},
+		},
+	},
+	plugins: [react()],
 });
