0fc1da4a99af5ee2d21d4f5dfbd84a7868b5cfe0
sungpaks <emforhs03150@gmail.com>
2024-08-05 13:49:05 +0900

    [feat] 사운드 컴포넌트 추가

---
+++ src/components/AudioPlayer.tsx
@@ -0,0 +1,38 @@
+import { resolve } from "path";
+import { useEffect, useRef, useState } from "react";
+
+export const SOUND = {
+	경적: "경적.mp3",
+	교통소음1: "교통소음1.mp3",
+	교통소음2: "교통소음2",
+	버스도착: "버스도착.mp3",
+	버스문열림: "버스문열림.mp3",
+	버스출발: "버스출발.mp3",
+	새소리: "새소리.mp3",
+	우당탕: "우당탕.mp3",
+	카운트: "카운트.mp3",
+	환호: "환호.mp3",
+	러닝발소리: "러닝발소리.mp3",
+	시나리오1_시작: "시나리오1_시작.mp3",
+};
+
+export default function AudioPlayer({
+	filename,
+	play,
+}: {
+	filename: string;
+	play: boolean;
+}) {
+	const audioRef = useRef<HTMLAudioElement>(null);
+	const url = resolve("/sounds/", filename);
+
+	useEffect(() => {
+		if (play && audioRef.current) {
+			audioRef.current.play();
+		} else if (audioRef.current) {
+			audioRef.current.pause();
+		}
+	}, [play, filename]);
+
+	return <audio src={url} ref={audioRef} />;
+}
