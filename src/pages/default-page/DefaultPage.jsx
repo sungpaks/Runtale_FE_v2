4cd358e1be9f9aeb0b3520f2ea61d06817c42959
doyun5565 <kdy__35@naver.com>
2024-09-15 11:00:15 +0900

    add : 바뀐 글씨체 추가, 디폴트 페이지 글씨체 변경

--- src/pages/default-page/DefaultPage.jsx
+++ src/pages/default-page/DefaultPage.jsx
@@ -10,6 +10,9 @@ export default function DefaultPage() {
 			<div className={`${styles["Content-Wrap"]}`}>
 				<Box
 					sx={{
+						backgroundImage: 'url("/path/to/Firefly King Arthur pulls the sword from the stone, surrounded by a crowd in awe, as the first rays .png")',
+						backgroundSize: 'cover',
+						opacity: 0.2,
 						width: 120,
 						height: 120,
 						display: "flex",
@@ -18,19 +21,18 @@ export default function DefaultPage() {
 						mb: 3,
 					}}
 				>
-					<img src="/img/로고.png" />
 				</Box>
 				<Typography
-					variant="h4"
-					sx={{ fontFamily: "RixInooAriDuriR", color: "#1899FF", mb: 1 }}
+					variant="h3"
+					sx={{fontFamily: "theSeason-bold", color: "#ECE3D7"}}
 				>
 					RUNTALE
 				</Typography>
 				<Typography
 					variant="body1"
-					sx={{ fontFamily: "Pretendard-regular", }}
+					sx={{ fontFamily: "Chosunilbo_myungjo", color : "#D5D5D5"}}
 				>
-					즐거운 러닝의 시작, 런테일!
+					즐거운 러닝의 시작, 런테일
 				</Typography>
 			</div>
 			<div className={`${styles["bottom-Wrap"]}`}>
@@ -39,9 +41,10 @@ export default function DefaultPage() {
 					disableElevation
 					sx={{
 						mt: 2,
-						width: "300px",
+						width: "234px",
 						height: "50px",
-						backgroundColor: "#1890FF",
+						backgroundColor: "#624925",
+						borderRadius: "100px",
 						color: "#FFFFFF",
 						fontWeight: "bold",
 						"&:hover": {
@@ -54,13 +57,14 @@ export default function DefaultPage() {
 					로그인
 				</Button>
 				<div className={`${styles["SignupWrap"]}`}>
-					<div className={`${styles["SignupText"]}`}>
+					<div className={`${styles["SignupText"]}`} style={{ fontFamily: "Chosunilbo_myungjo" }}>
 						아직 회원이 아니신가요?
 					</div>
 					<div className={`${styles["Signupbtnbox"]}`}>
 						<button
 							onClick={() => navigate("/signup")}
 							className={`${styles["Signupbtn"]}`}
+							style={{ fontFamily: "Chosunilbo_myungjo", color: "#F5B65DCC" }}
 						>
 							회원가입
 						</button>
