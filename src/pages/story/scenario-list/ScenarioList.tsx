64e7bb6fe500872fc9b32c72e4a5e1ecd4825ad8
sungpaks <emforhs03150@gmail.com>
2024-09-15 11:24:57 +0900

    [feat] 인기급상승시나리오 mock 프레임 작업

--- src/pages/story/scenario-list/ScenarioList.tsx
+++ src/pages/story/scenario-list/ScenarioList.tsx
@@ -1,16 +1,67 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import React from "react";
 import Title from "../../../components/Title";
 import styles from "./ScenarioList.module.css";
+import GradientCard from "../../../components/GradientCard";
+import profileImage0 from "../../../assets/scenario-profile-0.png";
+import profileImage1 from "../../../assets/scenario-profile-1.png";
+
+const mockScenarioList = [
+	{
+		profileImage: profileImage0,
+		title: "고전 1",
+		duration: 1,
+	},
+	{
+		profileImage: profileImage1,
+		title: "고전 2",
+		duration: 1,
+	},
+	{
+		profileImage: profileImage0,
+		title: "고전 3",
+		duration: 1,
+	},
+	{
+		profileImage: profileImage1,
+		title: "고전 4",
+		duration: 1,
+	},
+	{
+		profileImage: profileImage0,
+		title: "고전 5",
+		duration: 1,
+	},
+];
 
 export default function ScenarioList() {
 	return (
-		<Box className={styles["scenario-list-container"]} pt={5}>
-			<Title level={3}>
+		<Box className={styles["scenario-list-container"]} p={3} pt={5}>
+			<Title level={2}>
 				인기 급상승 시나리오{" "}
 				<span style={{ color: "#C27E1E" }}>TOP4</span>
 			</Title>
-			<Box height={"40vh"}></Box>
+			<Box className={styles["scenario-list"]} height={"40vh"}>
+				{mockScenarioList.map((item, index) => {
+					return (
+						<GradientCard
+							key={item.title}
+							height={224}
+							p={5}
+							sx={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+						>
+							<div>
+								<img src={item.profileImage} width="150px" />
+								<p style={{ color: "white" }}>{item.title}</p>
+								<br />
+								<button className={styles["start-button"]}>
+									시작하기
+								</button>
+							</div>
+						</GradientCard>
+					);
+				})}
+			</Box>
 		</Box>
 	);
 }
