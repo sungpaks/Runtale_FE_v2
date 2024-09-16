1cf10659cb75670023c3d7603b88ce936c7fa352
sungpaks <emforhs03150@gmail.com>
2024-09-16 21:06:27 +0900

    [feat] Render Props, Debouncing 등 이용한 검색 컴포넌트 기능 구현

---
+++ src/components/ScenarioListItem.tsx
@@ -0,0 +1,25 @@
+import React, { MouseEventHandler, useState } from "react";
+
+interface ScenarioListItemProps<T> {
+	buttonClassName: string;
+	item: any;
+	handleClickStart: MouseEventHandler;
+}
+
+export default function ScenarioListItem<T>({
+	buttonClassName,
+	item,
+	handleClickStart,
+}: ScenarioListItemProps<T>) {
+	const [searchTerm, setSearchTerm] = useState("");
+	return (
+		<div>
+			<img src={item.profileImage} width="120px" />
+			<p style={{ color: "white" }}>{item.title}</p>
+			<br />
+			<button className={buttonClassName} onClick={handleClickStart}>
+				시작하기
+			</button>
+		</div>
+	);
+}
